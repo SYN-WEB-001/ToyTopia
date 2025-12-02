5c4072f0cb0adf097399843158f2402da99171b6
Aref Saboor <arefsaboor.m@gmail.com>
2025-12-02 08:49:46 +0100

    Navbar sticky mode added

--- ToyTopia/src/components/Navbar.jsx
+++ ToyTopia/src/components/Navbar.jsx
@@ -8,15 +8,15 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-white shadow-lg">
+    <nav className="bg-white shadow-lg sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
+        <div className="flex justify-between items-center h-20">
           {/* Logo */}
           <div className="shrink-0">
             <img 
               src="/Logo.png" 
               alt="ToyTopia Logo" 
-              className="h-10 md:h-12 w-auto"
+              className="h-14 md:h-16 w-auto"
             />
           </div>
 
